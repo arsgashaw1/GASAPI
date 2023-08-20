e298c194d8595a8ad39c4de98e78ab640481edb7
Gashaw Tsegaye Shawo <coderman2011@gmail.com>
2023-08-20 07:47:24 +0200

    update

---
+++ src/main/java/com/gas/rest/Core/Rest/API/CoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.gas.rest.Core.Rest.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gas/rest/Core/Rest/API/CoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gas.rest.Core.Rest.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
